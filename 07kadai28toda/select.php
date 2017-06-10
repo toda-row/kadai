58e292e9b3ae8f9aa2fc1f246529f8cbcb920926
YOSHIHIRO <info@itjoho.com>
2017-06-10 13:47:12 +0900

    課題

---
+++ 07kadai28toda/select.php
@@ -0,0 +1,69 @@
+<?php
+//1.  DB接続します
+try {
+  $pdo = new PDO('mysql:dbname=gs_db28;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('データベースに接続できませんでした。'.$e->getMessage());
+}
+
+//２．データ登録SQL作成
+$stmt = $pdo->prepare("SELECT * FROM gs_bm_table");
+
+//baintvalue で検索
+
+$status = $stmt->execute();
+
+//３．データ表示
+$view="";
+if($status==false){
+  //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("ErrorQuery:".$error[2]);
+
+}else{
+  //Selectデータの数だけ自動でループしてくれる
+  while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $view .= '<tr>';//.=は追加処理＋＝と一緒
+    $view .= '<td>'.$result["id"].'</td>';
+    $view .= '<td>'.$result["bookname"].'</td>';
+    $view .= '<td>'.$result["bookurl"].'</td>';
+    $view .= '<td>'.$result["comment"].'</td>';
+    $view .= '</tr>';
+  }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>フリーアンケート表示</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">データ登録</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<table>
+    <div class="container jumbotron"><?=$view?></div>
+</table>
+<!-- Main[End] -->
+
+</body>
+</html>
