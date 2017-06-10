0acda37ccc26ca4f96472bf389dbe777a57fa41e
YOSHIHIRO <info@itjoho.com>
2017-06-10 18:14:47 +0900

    課題

---
+++ 08kadai28toda/detail.php
@@ -0,0 +1,72 @@
+<?php
+
+$id = $_GET["id"];
+
+//2. DB接続します
+try { //エラー入ったときに
+  $pdo = new PDO('mysql:dbname=gs_db28;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) { //受信します
+  exit('DbConnectError:'.$e->getMessage()); //エラー表示
+}
+
+
+//３．データ登録SQL変更
+$stmt = $pdo->prepare("SELECT * FROM gs_bm_table WHERE id=:id");
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute(); //executeは実行
+
+//データ表示
+$view="";
+if($status==false){
+  //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("ErrorQuery:".$error[2]);
+}else{
+    $row = $stmt->fetch();
+
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>データ登録</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body>
+
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <div class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<form method="post" action="update.php">
+ <input type="hidden" name="id" value="<?=$id?>">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>本をブックマークする</legend>
+     <label>書籍名：<input type="text" name="bookname" value="<?=$row["bookname"]?>"></label><br>
+     <label>書籍URL：<input type="text" name="bookurl" value="<?=$row["bookurl"]?>"></label><br>
+     <label><textArea name="comment" rows="4" cols="40"><?=$row["comment"]?></textArea></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+<!-- Main[End] -->
+
+
+</body>
+</html>
+
+
+
